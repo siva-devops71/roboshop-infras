4e8ca3fc3c9bcf83210ddf811516c584242e09a1
Siva Kumar <sivakumar9399959803@gmail.com>
2023-03-12 13:18:33 +0530

    learn in terraform

---
+++ ec2/main.tf
@@ -0,0 +1,71 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
+}
+
+
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.ec2.public_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "git clone https://github.com/siva-devops71/roboshop-shell.git",
+      "cd roboshop-shell",
+      "bash ${var.component}.sh ${var.password}"
+    ]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z04391752LS917TT76G1F"
+  name    = "${var.component}-dev.devops71.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
+variable "env" {
+  default = "dev"
+}
+variable "password" {}
